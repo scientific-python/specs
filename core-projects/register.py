ee38833c65804668b81007715341aacbea1ca491
Stefan van der Walt <stefanv@berkeley.edu>
2021-01-22 14:25:53 -0800

    Add default values to input; refactor input to tools

--- core-projects/register.py
+++ core-projects/register.py
@@ -5,35 +5,20 @@ SPE Registration
 Register your project as a SPE.
 """
 
-def input_(
-        prompt,
-        optional=False,
-        validate=lambda x: x if x else None
-):
-    def valid(s):
-        if optional and not s:
-            return ""
-        try:
-            return validate(s)
-        except (ValueError, TypeError):
-            print('Invalid input; please try again.')
-            return None
-
-    optional_flag = ' [optional]' if optional else ''
-    while (ans := valid(input(f'{prompt}{optional_flag}: '))) is None:
-        pass
-
-    return ans
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../.tools'))
+from tools import prompt
 
 
-project = input_("Project Name")
-homepage = input_("Project Homepage")
-decision = input_("Adopted by approval URL")
-repository = input_("Version control URL")
-pypi = input_("Project name on https://pypi.org/project/")
-license = input_("License URL")
-license_type = input_("License (e.g., 3-clause BSD, GPL)")
-adopters = input_("List the GitHub handles of developers who can adopt SPECs")
+project = prompt("Project Name")
+homepage = prompt("Project Homepage")
+decision = prompt("Adopted by approval URL")
+repository = prompt("Version control URL")
+pypi = prompt("Project name on https://pypi.org/project/")
+license = prompt("License URL")
+license_type = prompt("License (e.g., 3-clause BSD, GPL)")
+adopters = prompt("List the GitHub handles of developers who can adopt SPECs")
 
 filename = f"projects/{project.lower()}.md"
 text = f"""---
