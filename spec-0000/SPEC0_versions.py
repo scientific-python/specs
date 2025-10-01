scientific-python/specs

commit efc1f7624c46e68054d1d1f2f2a29c71f670ab82

Use ruff (#403)
Close #400.

- [ ] Decide what to do about `spec-0007/transition_to_rng.py` (ruff is
complaining)

spec-0000/SPEC0_versions.py
@@ -3,7 +3,7 @@ import collections
 from datetime import datetime, timedelta
 
 import pandas as pd
-from packaging.version import Version
+from packaging.version import Version, InvalidVersion
 
 
 py_releases = {
@@ -58,8 +58,8 @@ def get_release_dates(package, support_time=plus24):
         ver = f["filename"].split("-")[1]
         try:
             version = Version(ver)
-        except:
-            continue
+        except InvalidVersion as e:
+            print(f"Error: '{ver}' is an invalid version. Reason: {e}")
 
         if version.is_prerelease or version.micro != 0:
             continue
@@ -68,8 +68,8 @@ def get_release_dates(package, support_time=plus24):
         for format in ["%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ"]:
             try:
                 release_date = datetime.strptime(f["upload-time"], format)
-            except:
-                pass
+            except ValueError as e:
+                print(f"Error parsing invalid date: {e}")
 
         if not release_date:
             continue
@@ -168,7 +168,7 @@ def pad_table(table):
             if not width:
                 continue
             line += f"| {str.ljust(entry, width)} "
-        line += f"|"
+        line += "|"
         padded_table.append(line)
 
     return padded_table

.pre-commit-config.yaml
@@ -16,23 +16,19 @@ repos:
       - id: check-toml
       - id: check-added-large-files
 
-  - repo: https://github.com/psf/black
-    rev: 8a737e727ac5ab2f1d4cf5876720ed276dc8dc4b # frozen: 25.1.0
-    hooks:
-      - id: black
-
   - repo: https://github.com/rbubley/mirrors-prettier
-    rev: 787fb9f542b140ba0b2aced38e6a3e68021647a3 # frozen: v3.5.3
+    rev: 5ba47274f9b181bce26a5150a725577f3c336011 # frozen: v3.6.2
     hooks:
       - id: prettier
         files: \.(md|yml|yaml)
         args: [--prose-wrap=preserve]
 
-  - repo: https://github.com/asottile/pyupgrade
-    rev: ce40a160603ab0e7d9c627ae33d7ef3906e2d2b2 # frozen: v3.19.1
+  - repo: https://github.com/astral-sh/ruff-pre-commit
+    rev: 4cbc74d53fe5634e58e0e65db7d28939c9cec3f7 # frozen: v0.12.7
     hooks:
-      - id: pyupgrade
-        args: [--py38-plus]
+      - id: ruff
+        args: ["--fix", "--show-fixes", "--exit-non-zero-on-fix"]
+      - id: ruff-format
 
   - repo: https://github.com/codespell-project/codespell
     rev: "63c8f8312b7559622c0d82815639671ae42132ac" # frozen: v2.4.1

pyproject.toml
@@ -1,0 +1,3 @@
+[tool.ruff]
+target-version = "py313"
+exclude = ["spec-0007/transition_to_rng.py"]

commit 5a1fa2e1d98bdf3c016e8672a42779baeb29f9e4

BUG: fix spec0 script bug (#406)
The ruff changes introduced a bug in
efc1f7624c46e68054d1d1f2f2a29c71f670ab82 by removing the `continue` and
we haven't run the script since then to spot the issue.

spec-0000/SPEC0_versions.py
@@ -59,7 +59,8 @@ def get_release_dates(package, support_time=plus24):
         try:
             version = Version(ver)
         except InvalidVersion as e:
-            print(f"Error: '{ver}' is an invalid version. Reason: {e}")
+            print(f"Error: '{ver}' is an invalid version for '{package}'. Reason: {e}")
+            continue
 
         if version.is_prerelease or version.micro != 0:
             continue